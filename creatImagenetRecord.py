cd69abf79df937399eb260751370c9711e3d416f
Jiaxu Zhu <jiaxuzhu92@gmail.com>
2015-12-10 16:17:51 -0800

    ilsvrc2012 test

---
+++ creatImagenetRecord.py
@@ -0,0 +1,11 @@
+import os
+
+labels = open('ILSVRC2012_validation_ground_truth.txt', 'r')
+imageList =  open('ILSVRC2012_validation_image_list.txt', 'w')
+
+index = 1
+for line in labels.readlines():
+	label = int(line)
+	image_path = 'ILSVRC2012_val_%08d.JPEG' % index
+	imageList.write('%d\t%d\t%s\n' % (index, label, image_path))
+	index += 1
